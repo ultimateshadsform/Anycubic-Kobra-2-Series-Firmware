928a76732ce3a2ed21e3e29380619d8eee4835dc
Alexander Carlsson <alexxycarlsson@gmail.com>
2023-12-14 02:14:15 +0100

    Added a faster golang script to scan for firmware
    RIP THEIR SERVERS

---
+++ scripts/firmwares/go.mod
@@ -0,0 +1,3 @@
+module github.com/ultimateshadsform/Anycubic-Kobra-2-Series-Firmware
+
+go 1.21.5
--- scripts/firmwares/main.py
+++ scripts/firmwares/main.py
@@ -1,11 +1,10 @@
 import requests
 import threading
-import time
 
-base_url = "https://cdn.cloud-universe.anycubic.com/ota/K2Plus/AC104_K2Plus_1.1.0_{}.{}.{}_update.zip"
+base_url = "https://cdn.cloud-universe.anycubic.com/ota/K2Max/AC104_K2Max_{}.{}.{}_{}.{}.{}_update.zip"
 
 # Define the maximum number of simultaneous threads
-max_threads = 50
+max_threads = 100
 
 # Semaphore to limit the number of simultaneous threads
 semaphore = threading.Semaphore(max_threads)
@@ -14,10 +13,8 @@ semaphore = threading.Semaphore(max_threads)
 success_file = "successful_urls.txt"
 
 # Do requests with a delay between them
-
-
-def check_version(major, minor, patch):
-    url = base_url.format(major, minor, patch)
+def check_version(major1, minor1, patch1, major2, minor2, patch2):
+    url = base_url.format(major1, minor1, patch1, major2, minor2, patch2)
     with semaphore:
         r = requests.head(url)
         if r.status_code == 200:
@@ -29,18 +26,18 @@ def check_version(major, minor, patch):
         else:
             print(f"Failed: {url}")
 
-
 # Create threads
 threads = []
-for major in range(0, 10):
-    for minor in range(0, 10):
-        for patch in range(0, 10):
-            t = threading.Thread(target=check_version,
-                                 args=(major, minor, patch))
-            threads.append(t)
-            t.start()
-            # Add a small delay between starting threads
-            time.sleep(0.01)
+for major1 in range(0, 10):  # Change the range for the first major version
+    for minor1 in range(0, 10):  # Change the range for the first minor version
+        for patch1 in range(0, 10):
+            for major2 in range(0, 10):  # Change the range for the second major version
+                for minor2 in range(0, 10):  # Change the range for the second minor version
+                    for patch2 in range(0, 10):
+                        t = threading.Thread(target=check_version, args=(major1, minor1, patch1, major2, minor2, patch2))
+                        threads.append(t)
+                        t.start()
+                        # Add a small delay between starting threads
 
 # Wait for all threads to finish
 for t in threads:
